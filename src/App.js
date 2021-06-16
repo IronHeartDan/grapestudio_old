6290fec1dce2dac78c872fb0cebe527c13784f9b
Danish Khan <danishkhan.dk2.dk16@gmail.com>
2021-06-17 00:43:05 +0530

    Initial Services Commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Header from "./Header";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import Close from "@material-ui/icons/Close";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 function App() {
   const [drawerState, setDrawer] = useState(false);
   const [scrollTopState, setScrollTop] = useState(false);
@@ -59,188 +61,234 @@ function App() {
 
   return (
     <div className="app">
-      <div
-        class="overlay"
-        style={{
-          display: drawerState ? "block" : "none",
-          opacity: drawerState ? 1 : 0,
-        }}
-      >
-        <Button onClick={(e) => closeDrawer()}>
-          <Close />
-        </Button>
-      </div>
-      <div
-        className="scrollTop"
-        style={{ transform: scrollTopState ? "scale(1)" : "scale(0)" }}
-      >
-        <Button onClick={scrollToTop}>
-          <ArrowUpwardIcon />
-        </Button>
-      </div>
-      <Header
-        openDrawer={openDrawer}
-        closeDrawer={closeDrawer}
-        state={drawerState}
-      />
-      <Drawer state={drawerState} />
-      <div className="mainCon">
-        <div className="section">
-          <div className="carsouel" id="carsouel">
-            <img
-              src={svg1}
-              alt=""
-              style={{ opacity: carsouelView == 1 ? 1 : 0 }}
-            />
-            <img
-              src={svg2}
-              alt=""
-              style={{ opacity: carsouelView == 2 ? 1 : 0 }}
-            />
-            <img
-              src={svg3}
-              alt=""
-              style={{ opacity: carsouelView == 3 ? 1 : 0 }}
-            />
-          </div>
-          <div className="layerContent">
-            <div>
-              <h1>Demo Text</h1>
-              <p>
-                A big Demo Pice Of Text Just For The Sake Of Visual Purpouse!! A
-                big Demo Pice Of Text Just For The Sake Of Visual Purpouse!! A
-                big Demo Pice Of Text Just For The Sake Of Visual Purpouse!! A
-                big Demo Pice Of Text Just For The Sake Of Visual Purpouse!!
-              </p>
+      <Router>
+        <div
+          class="overlay"
+          style={{
+            display: drawerState ? "block" : "none",
+            opacity: drawerState ? 1 : 0,
+          }}
+        >
+          <Button onClick={(e) => closeDrawer()}>
+            <Close />
+          </Button>
+        </div>
+        <div
+          className="scrollTop"
+          style={{ transform: scrollTopState ? "scale(1)" : "scale(0)" }}
+        >
+          <Button onClick={scrollToTop}>
+            <ArrowUpwardIcon />
+          </Button>
+        </div>
+        <Header
+          openDrawer={openDrawer}
+          closeDrawer={closeDrawer}
+          state={drawerState}
+        />
+        <Drawer state={drawerState} />
+        <Switch>
+          <Route path="/services">
+            <div className="service_con">
+              <div>
+                <div>
+                  <div>
+                    <h1>Demo Servie Text Shown</h1>
+                    <ul>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <img src={svg1} alt="" />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <div>
+                    <h1>Demo Servie Text Shown</h1>
+                    <ul>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                      <li>Item</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <img src={svg1} alt="" />
+                </div>
+              </div>
+              <div>
+                <h1>Three</h1>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="section">
-          <div>
-            <h1>Our Services</h1>
-            <span
-              className={serviceView == 1 ? "servicesLinkActive" : ""}
-              onMouseOver={() => {
-                setService(1);
-              }}
-            >
-              Mobile Development
-            </span>
-            <span
-              className={serviceView == 2 ? "servicesLinkActive" : ""}
-              onMouseOver={() => {
-                setService(2);
-              }}
-            >
-              Website Development
-            </span>
-            <span
-              className={serviceView == 3 ? "servicesLinkActive" : ""}
-              onMouseOver={() => {
-                setService(3);
-              }}
-            >
-              Web / Mobile App Design
-            </span>
-            <span
-              className={serviceView == 4 ? "servicesLinkActive" : ""}
-              onMouseOver={() => {
-                setService(4);
-              }}
-            >
-              Graphic Designing
-            </span>
-            <span
-              className={serviceView == 5 ? "servicesLinkActive" : ""}
-              onMouseOver={() => {
-                setService(5);
-              }}
-            >
-              Trending Technology
-            </span>
-            <span
-              className={serviceView == 6 ? "servicesLinkActive" : ""}
-              onMouseOver={() => {
-                setService(6);
-              }}
-            >
-              Others
-            </span>
-          </div>
-          <div>
-            {(() => {
-              switch (serviceView) {
-                case 1:
-                  return (
-                    <>
-                      <a href="">One</a>
-                    </>
-                  );
-                  break;
+          </Route>
+          <Route path="/">
+            <div className="mainCon">
+              <div className="section">
+                <div className="carsouel" id="carsouel">
+                  <img
+                    src={svg1}
+                    alt=""
+                    style={{ opacity: carsouelView == 1 ? 1 : 0 }}
+                  />
+                  <img
+                    src={svg2}
+                    alt=""
+                    style={{ opacity: carsouelView == 2 ? 1 : 0 }}
+                  />
+                  <img
+                    src={svg3}
+                    alt=""
+                    style={{ opacity: carsouelView == 3 ? 1 : 0 }}
+                  />
+                </div>
+                <div className="layerContent">
+                  <div>
+                    <h1>Demo Text</h1>
+                    <p>
+                      A big Demo Pice Of Text Just For The Sake Of Visual
+                      Purpouse!! A big Demo Pice Of Text Just For The Sake Of
+                      Visual Purpouse!! A big Demo Pice Of Text Just For The
+                      Sake Of Visual Purpouse!! A big Demo Pice Of Text Just For
+                      The Sake Of Visual Purpouse!!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="section">
+                <div>
+                  <h1>Our Services</h1>
+                  <span
+                    className={serviceView == 1 ? "servicesLinkActive" : ""}
+                    onClick={() => {
+                      setService(1);
+                    }}
+                  >
+                    Mobile Development
+                  </span>
+                  <span
+                    className={serviceView == 2 ? "servicesLinkActive" : ""}
+                    onClick={() => {
+                      setService(2);
+                    }}
+                  >
+                    Website Development
+                  </span>
+                  <span
+                    className={serviceView == 3 ? "servicesLinkActive" : ""}
+                    onClick={() => {
+                      setService(3);
+                    }}
+                  >
+                    Web / Mobile App Design
+                  </span>
+                  <span
+                    className={serviceView == 4 ? "servicesLinkActive" : ""}
+                    onClick={() => {
+                      setService(4);
+                    }}
+                  >
+                    Graphic Designing
+                  </span>
+                  <span
+                    className={serviceView == 5 ? "servicesLinkActive" : ""}
+                    onClick={() => {
+                      setService(5);
+                    }}
+                  >
+                    Trending Technology
+                  </span>
+                  <span
+                    className={serviceView == 6 ? "servicesLinkActive" : ""}
+                    onClick={() => {
+                      setService(6);
+                    }}
+                  >
+                    Others
+                  </span>
+                </div>
+                <div>
+                  {(() => {
+                    switch (serviceView) {
+                      case 1:
+                        return <Link to="/services">One</Link>;
+                        break;
 
-                case 2:
-                  return (
-                    <>
-                      <a href="">One</a>
-                      <a href="">Two</a>
-                    </>
-                  );
-                  break;
+                      case 2:
+                        return (
+                          <>
+                            <Link to="/services">One</Link>
+                            <Link to="/services">Two</Link>
+                          </>
+                        );
+                        break;
 
-                case 3:
-                  return (
-                    <>
-                      <a href="">One</a>
-                      <a href="">Two</a>
-                      <a href="">Three</a>
-                    </>
-                  );
-                  break;
+                      case 3:
+                        return (
+                          <>
+                            <Link to="/services">One</Link>
+                            <Link to="/services">Two</Link>
+                            <Link to="/services">Three</Link>
+                          </>
+                        );
+                        break;
 
-                case 4:
-                  return (
-                    <>
-                      <a href="">One</a>
-                      <a href="">Two</a>
-                      <a href="">Three</a>
-                      <a href="">Four</a>
-                    </>
-                  );
-                  break;
+                      case 4:
+                        return (
+                          <>
+                            <Link to="/services">One</Link>
+                            <Link to="/services">Two</Link>
+                            <Link to="/services">Three</Link>
+                            <Link to="/services">Four</Link>
+                          </>
+                        );
+                        break;
 
-                case 5:
-                  return (
-                    <>
-                      <a href="">One</a>
-                      <a href="">Two</a>
-                      <a href="">Three</a>
-                      <a href="">Four</a>
-                      <a href="">Five</a>
-                    </>
-                  );
-                  break;
+                      case 5:
+                        return (
+                          <>
+                            <Link to="/services">One</Link>
+                            <Link to="/services">Two</Link>
+                            <Link to="/services">Three</Link>
+                            <Link to="/services">Four</Link>
+                            <Link to="/services">Fve</Link>
+                          </>
+                        );
+                        break;
 
-                case 6:
-                  return (
-                    <>
-                      <a href="">One</a>
-                      <a href="">Two</a>
-                      <a href="">Three</a>
-                      <a href="">Four</a>
-                      <a href="">Five</a>
-                      <a href="">Six</a>
-                    </>
-                  );
-                  break;
-                default:
-                  return null;
-              }
-            })()}
-          </div>
-        </div>
-        <div className="section">
-          <h1>Footer</h1>
-        </div>
+                      case 6:
+                        return (
+                          <>
+                            <Link to="/services">One</Link>
+                            <Link to="/services">Two</Link>
+                            <Link to="/services">Three</Link>
+                            <Link to="/services">Four</Link>
+                            <Link to="/services">Fve</Link>
+                            <Link to="/services">Six</Link>
+                          </>
+                        );
+                        break;
+                      default:
+                        return null;
+                    }
+                  })()}
+                </div>
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+      <div className="footer">
+        <h1>Footer</h1>
       </div>
     </div>
   );
